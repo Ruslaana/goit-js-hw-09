61d0056066973d5d3e18e6b5bb42213cacaa0359
Ruslaana <119338930+Ruslaana@users.noreply.github.com>
2023-05-20 20:26:00 +0000

    Deploying to gh-pages from @ Ruslaana/goit-js-hw-09@e36e43043e25bc334d44a168eb2a166478fc442e 🚀

---
+++ 01-color-switcher.514ec97f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=document.body;let a=null;t.addEventListener("click",(function(){a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.514ec97f.js.map
