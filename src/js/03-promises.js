7f3f0ce9c88450301704525d8818f6be1674dc39
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-05-20 22:49:56 +0200

    task 3 , not finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    createPromise(position, delay)
+      .then(result => {
+        console.log(
+          `Promise ${result.position} fulfilled after ${result.delay} ms`
+        );
+      })
+      .catch(result => {
+        console.log(
+          `Promise ${result.position} rejected after ${result.delay} ms`
+        );
+      });
+
+    delay += step;
+  }
+});
