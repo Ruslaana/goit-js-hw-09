e36e43043e25bc334d44a168eb2a166478fc442e
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-05-20 22:25:40 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+let intervalId = null;
+
+function startColorSwitch() {
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+  }, 1000);
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+function stopColorSwitch() {
+  clearInterval(intervalId);
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.addEventListener('click', startColorSwitch);
+stopButton.addEventListener('click', stopColorSwitch);
